f395af4c434e01be0d3803efd44217dcd471123e
Gregory Vandenbroucke <gvdenbro@users.noreply.github.com>
2023-05-15 13:45:26 +0000

    blocking cookie consent

--- tests/buienradar.spec.ts
+++ tests/buienradar.spec.ts
@@ -3,18 +3,25 @@ import { removeDirectory } from './fs-utils';
 
 const brScrapesDirectory: string = './scrapes/buienradar'
 
-test("buienradar jette 3 uur", async ({ page }) => {
+test("buienradar jette 3 uur", async ({ page, context }) => {
+  
+  await context.route("**/*", (request) => { // block cookie consent popup
+    request.request().url().startsWith("https://cdn.cookielaw.org")
+      ? request.abort()
+      : request.continue();
+    return;
+  });
 
   await page.goto("https://www.buienradar.be/weer/jette/be/2794914/buienradar/3uurs");
-  
+
   const popup = page.getByText('Nu niet, misschien later');
-  
+
   await expect(popup).toBeVisible();
   // get rid of popup
   await popup.click();
 
   const noRainLabel = page.locator('.categoryLabel', { hasText: 'Geen neerslag verwacht' });
-  
+
   if (await noRainLabel.isVisible()) {
     removeDirectory(brScrapesDirectory);
     return;
