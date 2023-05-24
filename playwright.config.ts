3fb405da2b6eb9b9c0ee664b6b234b617feec4a9
gerg <gerg@ec.eu>
2023-05-24 16:05:19 +0200

    increasing viewport for the screenshots

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,7 +42,10 @@ export default defineConfig({
 
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: {
+        ...devices['Desktop Firefox'],
+        viewport: { width: 1920, height: 1080 },
+      },
     },
 
     {
