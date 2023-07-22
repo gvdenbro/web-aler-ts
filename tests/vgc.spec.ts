6ada8f84171900bb0bcd985d43dda46fa14d2b6a
gerg <gerg@ec.eu>
2023-07-22 15:52:27 +0200

    fixing vgc when there are no results

--- tests/vgc.spec.ts
+++ tests/vgc.spec.ts
@@ -31,59 +31,46 @@ for (const dienst of diensten) {
 
     await page.goto(`https://tickets.vgc.be/activity/index?&vrijeplaatsen=1&Age%5B%5D=${age - 1}%2C${age + 1}&entity=${dienst.id}`);
 
-    const results = page.locator('#wall > .items');
+    const results = page.locator('#wall');
 
     await expect(results).toBeVisible();
 
-    const links = results.getByRole('link').filter({hasNotText: 'Lees meer'});
+    const items = results.locator('.items');
 
-    const activities = await links.all();
+    if (await items.count() > 0) {
 
-    for (const activity of activities) {
-
-      await activity.click();
-
-      const mainContent = await page.locator('#main-content');
-
-      await expect(mainContent).toBeVisible();
-
-      const metaTag = page.locator("meta[property='og:url']");
-
-      if (await metaTag.count() > 0) {
-
-        const ogUrl = await metaTag.getAttribute("content");
-
-        expect(ogUrl).toBeDefined();
-
-        await page.evaluate(() => {
-          document.querySelectorAll(".badge").forEach(el => el.remove());
-        });
-
-        const htmlContent = await mainContent.innerHTML();
-
-        createMarkdown(`${scrapesDirectory}/${dienst.naam}/${ogUrl}.md`, `<div><div>${htmlContent}</div><p><a href="${page.url()}">Source</a></p></div>`);
+      const links = items.getByRole('link').filter({hasNotText: 'Lees meer'});
+  
+      const activities = await links.all();
+  
+      for (const activity of activities) {
+  
+        await activity.click();
+  
+        const mainContent = await page.locator('#main-content');
+  
+        await expect(mainContent).toBeVisible();
+  
+        const metaTag = page.locator("meta[property='og:url']");
+  
+        if (await metaTag.count() > 0) {
+  
+          const ogUrl = await metaTag.getAttribute("content");
+  
+          expect(ogUrl).toBeDefined();
+  
+          await page.evaluate(() => {
+            document.querySelectorAll(".badge").forEach(el => el.remove());
+          });
+  
+          const htmlContent = await mainContent.innerHTML();
+  
+          createMarkdown(`${scrapesDirectory}/${dienst.naam}/${ogUrl}.md`, `<div><div>${htmlContent}</div><p><a href="${page.url()}">Source</a></p></div>`);
+        }
+  
+        page.context().clearCookies(); // clear cookies because otherwise we end up in an error because of some bug on the website
+        await page.goBack();
       }
-
-      page.context().clearCookies(); // clear cookies because otherwise we end up in an error because of some bug on the website
-      await page.goBack();
     }
-
   });
 }
-
-// https://www.essegem.be/activiteiten/ketjesatelier-0
-
-test("ketjes-atelier", async ({ page, context }, testInfo) => {
-
-  await page.goto("https://www.essegem.be/activiteiten/ketjesatelier-0");
-
-  const mainContent = page.locator('#content .activity-left');
-
-  await expect(mainContent).toBeVisible();
-
-  const htmlContent = await mainContent.innerHTML();
-
-  createMarkdown(`${scrapesDirectory}/essegem/${testInfo.title}.md`, `<div><div>${htmlContent}</div><p><img src="${testInfo.title}.png"></img></p><p><a href="${page.url()}">Source</a></p></div>`);
-
-  await mainContent.screenshot({ path: `${scrapesDirectory}/essegem/${testInfo.title}.png` });
-});
