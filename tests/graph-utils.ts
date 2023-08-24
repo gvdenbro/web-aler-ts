947d61378341f9bb60b9e61ed5b81ef817aac48a
gerg <gerg@ec.eu>
2023-08-24 15:32:44 +0200

    png

--- tests/graph-utils.ts
+++ tests/graph-utils.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import { TopLevelSpec, compile } from 'vega-lite';
 import { Loader, loader, read, parse, View } from 'vega';
+import svg2img from 'svg2img'
 
 export function generateSvg(dataFilePath: string, svgFilePath: string, dataParseConfig: any, vegaLiteSpec: (data: object[]) => TopLevelSpec): void {
 
@@ -13,6 +14,12 @@ export function generateSvg(dataFilePath: string, svgFilePath: string, dataParse
     const view = new View(parse(vegaSpec), { renderer: 'none' });
     view.toSVG().then(svg => {
       fs.writeFileSync(svgFilePath, svg);
+    });
+    // hack in order to have a png version for telegram
+    // vega-lite can generate png with the help of a 'canvas' lib, but results are not great
+    // so using vega-lite to export to svg and use svg2img to convert to png 🤮
+    svg2img(svgFilePath, function (error, buffer) {
+      fs.writeFileSync(svgFilePath.replace('.svg', '.png'), buffer);
     });
   });
 }
