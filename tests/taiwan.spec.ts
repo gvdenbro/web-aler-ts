147a119564691cf07d0444efbb534621ccd6f45d
gerg <gerg@ec.eu>
2024-06-27 10:59:59 +0000

    adding singapore and devcontainer ui support

--- tests/taiwan.spec.ts
+++ tests/taiwan.spec.ts
@@ -71,31 +71,31 @@ test('turkish-airlines', async ({ page }) => {
     await page.locator('#toPort').click();
     await page.locator('#toPort').clear();
     // await expect(page.locator('#destinationSelector').getByText('See all destinations')).toBeVisible();
-    await page.locator('#toPort').type('TPE', {delay: 300});
+    await page.locator('#toPort').type('TPE', { delay: 300 });
     await page.getByText('(TPE)').first().click();
-    
+
     await page.locator('#flexibleDates').click();
-    
+
     while (! await page.getByText('April 2025').isVisible()) {
         // await page.getByTitle('Next').first().click();
         await page.getByRole('button', { name: '›' }).click()
     }
-    
+
     await page.getByLabel('April 5, 2025').click();
 
     // await page.getByTitle('Next').first().click();
     // await expect(page.getByText('April 2024')).toBeVisible();
     await page.getByLabel('April 20, 2025').click();
-    
+
     // await page.getByText('NextApril 2025MoTuWeThFrSaSu123456789101112131415161718192021222324252627282930').getByRole('link', { name: '20' }).click();
-    
+
     await page.getByRole('button', { name: 'OK', exact: true }).click();
-    
+
     await page.locator('#fromPort').click();
     await page.locator('#fromPort').clear();
-    
+
     // await expect(page.locator('#originSelector').getByText('See all destinations')).toBeVisible();
-    await page.locator('#fromPort').type('BRU', {delay: 300});
+    await page.locator('#fromPort').type('BRU', { delay: 300 });
     await page.getByText('(BRU)').first().click();
 
     await page.getByRole('button', { name: 'Search flights' }).click();
@@ -111,4 +111,41 @@ test('turkish-airlines', async ({ page }) => {
     });
 
     createMarkdown(`${scrapesDirectory}/turkish-airlines.md`, `<table>${await gridResultPage.innerHTML()}</table><img src="turkish-airlines.png"></img>`, { handleTables: true });
+});
+
+test('singapore-airlines', async ({ page, context }) => {
+
+    await page.setViewportSize({ width: 1920, height: 1080 });
+
+    context.addCookies([
+        { name: "ALLOW_TO_USE_COOKIES", value: "ALLOWED", domain: "www.singaporeair.com", path: "/" },
+        { name: "AKAMAI_SAA_LOCALE_COOKIE", value: "en_UK", domain: "www.singaporeair.com", path: "/" },
+        { name: "AKAMAI_SAA_COUNTRY_COOKIE", value: "BE", domain: "www.singaporeair.com", path: "/" }
+    ]);
+
+    await page.goto('https://www.singaporeair.com/en_UK/be/home#/book/bookflight');
+
+    await page.getByLabel('From').click();
+    await page.getByLabel('From').clear();
+    await page.getByLabel('From').fill('BRU');
+    await page.getByText('BRU', { exact: true }).click();
+
+    await page.getByLabel('To').click();
+    await page.getByLabel('To').clear();
+    await page.getByLabel('To').fill('TPE');
+    await page.getByText('TPE', { exact: true }).click();
+
+    await page.getByLabel('Depart Date').click();
+
+    while (! await page.getByText('april 2025').isVisible()) {
+        await page.locator('.right').first().click();
+    }
+
+    await page.getByText('5No flights operating', { exact: true }).nth(1).click();
+    await page.getByText('20No flights operating').nth(1).click();
+    await page.getByRole('button', { name: 'Done' }).click();
+
+    await page.getByRole('button', { name: 'Search' }).click();
+
+    await page.locator('.select-flight__flight-block').first();
 });
