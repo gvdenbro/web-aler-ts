822763e12e181b4d5677224b81ffd6a4d1f5409c
gerg <gerg@ec.eu>
2024-12-13 16:08:28 +0000

    adding digi check

---
+++ tests/apis.spec.ts
@@ -0,0 +1,41 @@
+import { test, expect } from '@playwright/test';
+import { removeFiles } from './fs-utils';
+import { createMarkdown } from './md-utils';
+
+const scrapesDirectory: string = './scrapes/apis'
+
+test.beforeAll(async ({ }, testInfo) => {
+    if (!testInfo.retry) { // on failure workers can be restarted and then beforeAll called again which might mess up the directory cleaning
+      removeFiles(scrapesDirectory, 'md');
+      removeFiles(scrapesDirectory, 'png');
+    }
+  });
+
+test('digi belgium available', async ({ request }, testInfo) => {
+
+    const response = await request.put('https://api.digi-belgium.be/v1/address/infrastructure', {
+      data: {
+        cityId: 21010,
+        countryId: 99,
+        streetId: 63259,
+        fttbAddressId: "E6C156B2-095A-EF11-A2CB-001DD8B7203E",
+        postalCode: "1090",
+        city: "Jette",
+        street:"Esseghemstraat",
+        number:"34",
+        box:"",
+        platform:"web",
+        email:"blahblah@car.net",
+        agree:false
+      },
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    const jsonResponse = await response.json();
+
+    expect(jsonResponse).toBeTruthy();
+
+    createMarkdown(`${scrapesDirectory}/${testInfo.title}.md`, `<div><pre><code>${JSON.stringify(jsonResponse)}</code></pre></div>`);
+  });
