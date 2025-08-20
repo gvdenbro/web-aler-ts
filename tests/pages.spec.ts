5c40e74b2c46ff06b5067496a1942d1198abb659
Gregory Vandenbroucke <gvdenbro@users.noreply.github.com>
2025-08-20 06:38:45 +0200

    re-enabling canzoniere

--- tests/pages.spec.ts
+++ tests/pages.spec.ts
@@ -15,6 +15,6 @@ test.beforeEach(async ({ context }) => {
     await context.route(/(.*forter.*)|(.*google.*)|(.*amplitude.*)|(.*powerreviews.*)|(.*cquotient.*)|(.*dynamicyield.*)|(.*yottaa.*)/, route => route.abort());
 });
 
-//testPage("canzonieregrecanicosalentino", "https://canzonieregrecanicosalentino.net/tour/", page => page.getByRole('article'), scrapesDirectory);
+testPage("canzonieregrecanicosalentino", "https://canzonieregrecanicosalentino.net/tour/", page => page.getByRole('article'), scrapesDirectory);
 testPage("mariamazzotta", "https://www.mariamazzotta.com/tour-2/", page => page.getByRole('article'), scrapesDirectory);
 testPage("maximvengerov", "https://www.maximvengerov.com/events", page => page.locator('.sqs-events-collection-list'), scrapesDirectory);
