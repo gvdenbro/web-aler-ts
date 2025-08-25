b3810d3dfdc253b8ed8d82b65df6eee79ee1f77a
gerg <gerg@ec.eu>
2025-08-25 14:34:35 +0000

    adding stuff

--- tests/pages.spec.ts
+++ tests/pages.spec.ts
@@ -12,9 +12,13 @@ test.beforeAll(async ({ }, testInfo) => {
 });
 
 test.beforeEach(async ({ context }) => {
-    await context.route(/(.*forter.*)|(.*google.*)|(.*amplitude.*)|(.*powerreviews.*)|(.*cquotient.*)|(.*dynamicyield.*)|(.*yottaa.*)/, route => route.abort());
+    await context.route(/(.*forter.*)|(.*google.*)|(.*amplitude.*)|(.*powerreviews.*)|(.*cquotient.*)|(.*dynamicyield.*)|(.*yottaa.*)|(.*cookie.*)/, route => route.abort());
 });
 
 testPage("canzonieregrecanicosalentino", "https://canzonieregrecanicosalentino.net/tour/", page => page.getByRole('article'), scrapesDirectory);
 testPage("mariamazzotta", "https://www.mariamazzotta.com/tour-2/", page => page.getByRole('article'), scrapesDirectory);
 testPage("maximvengerov", "https://www.maximvengerov.com/events", page => page.locator('.sqs-events-collection-list'), scrapesDirectory);
+
+testPage("action-yoga-mat", "https://www.action.com/fr-be/p/2567007/tapis-de-yoga-kaytan/", page => page.getByRole('region', { name: 'Description de l\'article' }), scrapesDirectory);
+testPage("lidl-yoga-mat", "https://www.lidl.be/p/fr-BE/schildkrot-fitness-tapis-de-fitness/p100290653", page => page.locator('article.detail-one > div > div').nth(1), scrapesDirectory);
+testPage("lidl-porte-velo", "https://www.lidl.be/p/fr-BE/pied-d-atelier-pour-velo-crivit/p100386064", page => page.locator('article.detail-one > div > div').nth(1), scrapesDirectory);
