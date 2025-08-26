879f0b83ee54bad99cef5a707233efaf5a6cc1db
gerg <gregory.vandenbroucke@gmail.com>
2025-08-26 06:15:53 +0200

    trying real blabla stuff

--- tests/pages.spec.ts
+++ tests/pages.spec.ts
@@ -20,5 +20,5 @@ testPage("mariamazzotta", "https://www.mariamazzotta.com/tour-2/", page => page.
 testPage("maximvengerov", "https://www.maximvengerov.com/events", page => page.locator('.sqs-events-collection-list'), scrapesDirectory);
 
 testPage("action-yoga-mat", "https://www.action.com/fr-be/p/2567007/tapis-de-yoga-kaytan/", page => page.getByRole('region', { name: 'Description de l\'article' }), scrapesDirectory);
-testPage("lidl-yoga-mat", "https://www.lidl.be/p/fr-BE/schildkrot-fitness-tapis-de-fitness/p100290653", page => page.locator('article.detail-one > div > div').nth(1), scrapesDirectory);
-testPage("lidl-porte-velo", "https://www.lidl.be/p/fr-BE/pied-d-atelier-pour-velo-crivit/p100386064", page => page.locator('article.detail-one > div > div').nth(1), scrapesDirectory);
+//testPage("lidl-yoga-mat", "https://www.lidl.be/p/fr-BE/schildkrot-fitness-tapis-de-fitness/p100290653", page => page.locator('article.detail-one > div > div').nth(1), scrapesDirectory);
+//testPage("lidl-porte-velo", "https://www.lidl.be/p/fr-BE/pied-d-atelier-pour-velo-crivit/p100386064", page => page.locator('article.detail-one > div > div').nth(1), scrapesDirectory);
